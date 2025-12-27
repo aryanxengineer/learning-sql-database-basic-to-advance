576fbc19d339c4ac6c6bf5eb4a3db32bac233feb
aryanxengineer <aryanxengineer@gmail.com>
2025-12-27 21:58:53 +0530

    I am learning sql joins

---
+++ joins/basic/combination.sql
@@ -0,0 +1,28 @@
+/*
+
+    Everything about COMBINATIONS:-
+    
+    It can be two types:-
+        columns -> JOINS
+        rows -> SET OPERATORS
+
+    Types of joins:- We have to define some Key Columns in case of JOINS
+        Inner
+        Full
+        Left
+        Right
+
+    What is JOINS
+
+    When to use JOINS
+
+    
+
+    Types of SET OPERATORS:- Columns numbers should be same in case of SET Operators
+        Union
+        Union All
+        Except
+        Intersect
+
+
+*/
