a2173f405f0dc883797879bbe4cacc9f0a22a804
aryanxengineer <aryanxengineer@gmail.com>
2026-01-01 16:36:26 +0530

    left anti joi is completed today

--- joins/advance/leftantijoin.sql
+++ joins/advance/leftantijoin.sql
@@ -6,3 +6,13 @@
     So we can define in such a way -> left table is known as primary source of data and second table is using for filtering the data 
 
 */
+
+
+-- Get all customers who haven't place any order
+SELECT 
+    *
+FROM customers
+LEFT JOIN orders
+ON id = customer_id
+WHERE customer_id IS NULL;
+
